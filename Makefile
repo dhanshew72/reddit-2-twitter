59affa50f9fcc107ee1499fd96dc8ceea3cd2007
dhanshew72 <d.hanshew@aol.com>
2022-06-05 19:32:24 -0700

    Initialized repo

--- Makefile
+++ Makefile
@@ -5,11 +5,3 @@ dev-env: clean
 
 clean:
 	rm -rf .venv
-
-run:
-	$(ACTIVATE_VENV); PYTHONPATH=src CONFIG_FILE=src/config-prod.ini python main.py
-
-refresh-token:
-	# This command will pop up a URL that will take you to a page to hit accept.
-    # The page will error out, but come back to the CLI and you'll have the token.
-	$(ACTIVATE_VENV); PYTHONPATH=src python src/reddit/authorize.py $(CLIENT_ID) $(CLIENT_SECRET) $(USERNAME)
--- main.py
+++ main.py
@@ -1,29 +1,7 @@
-from images.download import Downloader
-from images.clean import Cleaner
-from reddit.read import Reader
-from twitter.write import Writer
-from config import Config
 
 
 def main():
-    config = Config()
-    path_prefix = config.read('DOWNLOADER')['path_prefix']
-    posts = Reader(config.read('REDDIT'), "memes").read()
-    files = Downloader(path_prefix).batch(filter_gifs(posts))
-    Writer(config.read('TWITTER')).batch(files)
-    Cleaner(path_prefix).batch(files)
-
-
-def filter_gifs(posts):
-    jpegs = []
-    # Only grabbing top 3 jpg posts for now otherwise just all jpg posts,
-    # if anything more comes up I'll separate this out into a module for filtering
-    for post in posts:
-        if post.split('.')[-1] == 'jpg':
-            jpegs.append(post)
-        if len(jpegs) >= 3:
-            break
-    return jpegs
+    pass
 
 
 if __name__ == '__main__':
